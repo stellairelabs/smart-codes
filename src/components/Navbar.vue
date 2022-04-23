70b37ccd4c281f33716a451865b63599ac4503bd
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-04-23 17:12:06 +0530

    added navbar and configured navbar in mobile view

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <v-img class="shrink mr-" contain :src="logo" width="40" />
+        </router-link>
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer = true" class="hidden-md-and-up"></v-app-bar-nav-icon>
+
+      <div class="hidden-sm-and-down">
+        <v-btn v-for="(item, i) in menuItems" :key="i" :to="item.path" text rounded large>
+          {{ item.title }}
+        </v-btn>
+        <v-btn to="/request-a-quote" text outlined x-large> Request A Quote </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item v-for="(item, i) in menuItems" :key="i" :to="item.path">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+
+          <v-btn to="/request-a-quote" text outlined> Request A Quote </v-btn>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import logo from "../assets/logo.png";
+export default {
+  name: "Navbar",
+  data: () => ({
+    logo,
+    menuItems: [
+      { title: "Home", path: "/" },
+      { title: "Services", path: "/services" },
+      { title: "About", path: "/about" },
+      { title: "Blog", path: "/blog" },
+    ],
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style></style>
