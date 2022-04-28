c2e8a96a6518bb2c5dd11b131b1e5ab9a8fa5470
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-04-28 21:46:05 +0530

    added basic styling in home page

---
+++ src/components/home/Jumbotron.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-img :src="jumbotron.img" aspect-ratio="3">
+      <h1>{{ jumbotron.title1 }}<br />{{ jumbotron.title2 }}</h1>
+      <p>{{ jumbotron.text }}</p></v-img
+    >
+  </v-row>
+</template>
+
+<script>
+import { jumbotron } from "../../data/home";
+export default {
+  name: "Jumbotron",
+  data: () => ({
+    jumbotron,
+  }),
+};
+</script>
+
+<style></style>
--- src/components/home/OurServices.vue
+++ src/components/home/OurServices.vue
@@ -1,28 +1,26 @@
 <template>
   <v-row class="our-services">
     <v-col cols="12">
-      <h2>Our Services</h2>
+      <h2 class="our-services__title">Our Services</h2>
     </v-col>
     <v-col v-for="(category, i) in services" :key="i">
-      <h3>{{ category.category }}</h3>
+      <h3 class="our-services__subtitle">{{ category.category }}</h3>
       <v-list>
-        <v-list-item-group>
-          <v-list-item v-for="(service, j) in category.services" :key="j">
-            <!-- <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon> -->
-            <v-list-item-content>
-              <v-list-item-title v-text="service.title"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
+        <v-list-item v-for="(service, j) in category.services" :key="j">
+          <v-list-item-icon>
+            <v-icon>mdi-check-circle</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="service.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import services from "../../data/services.json";
+import { services } from "../../data/services.js";
 export default {
   name: "OurServices",
   data: () => ({
@@ -31,4 +29,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.our-services__title {
+  text-align: center;
+  font-weight: bold;
+}
+
+.our-services__subtitle {
+  text-align: center;
+  text-decoration: underline;
+}
+</style>
