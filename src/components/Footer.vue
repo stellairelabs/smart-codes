93e532792bd39144ec26a55370c29c24de773663
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-04-30 11:19:14 +0530

    footer data added

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-footer padless class="footer">
+    <v-container>
+      <v-row>
+        <v-col
+          ><v-img max-height="150" max-width="250" contain :src="logo"></v-img>
+          <p></p></v-col
+        ><v-col></v-col><v-col></v-col><v-col></v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+import logo from "../assets/logo.png";
+export default {
+  name: "Footer",
+  data: () => ({
+    logo,
+  }),
+};
+</script>
+
+<style></style>
