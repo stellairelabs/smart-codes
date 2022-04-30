a10fbbcdbefb243d0ca451fbc52eeb7dcca2b32a
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-04-30 11:19:14 +0530

    footer data added

---
+++ src/data/navAndFooter.js
@@ -0,0 +1,30 @@
+export const logo = require("../assets/logo.png"),
+  menuItems = [
+    { title: "Home", path: "/" },
+    { title: "Services", path: "/services" },
+    { title: "About", path: "/about" },
+    { title: "Blog", path: "/blog" },
+  ],
+  socialmedia = [
+    {
+      social: "Facebook",
+      icon: "",
+      link: "",
+    },
+    {
+      social: "Facebook",
+      icon: "",
+      link: "",
+    },
+    {
+      social: "Facebook",
+      icon: "",
+      link: "",
+    },
+    {
+      social: "Facebook",
+      icon: "",
+      link: "",
+    },
+  ],
+  footerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis";
